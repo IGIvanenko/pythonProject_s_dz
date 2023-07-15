5fa85e3ee8b3832f814c85048cce7370eb9a3a96
IGIvanenko <109788100+IGIvanenko@users.noreply.github.com>
2023-07-15 12:16:38 +0300

    Add files via upload

---
+++ second/venv_1/fractions.py
@@ -0,0 +1,13 @@
+def process_fractions(frac1_str, frac2_str):
+    num1, denom1 = map(int, frac1_str.split("/"))
+    num2, denom2 = map(int, frac2_str.split("/"))
+                                                    #сумма
+    sum_frac_num = num1 * denom2 + num2 * denom1
+    sum_frac_denom = denom1 * denom2
+    sum_frac = (sum_frac_num, sum_frac_denom)
+
+    prod_frac_num = num1 * num2                      #произведение
+    prod_frac_denom = denom1 * denom2
+    prod_frac = (prod_frac_num, prod_frac_denom)
+
+    return sum_frac, prod_frac
---
+++ second/venv_1/hex.py
@@ -0,0 +1,9 @@
+if __name__ == '__main__':
+    i = 450
+
+    h = hex(i)
+    print(h)
+
+
+num = 450
+print(num, 'in hex =', hex(num))
